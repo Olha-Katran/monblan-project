294f57522f8ece46311c7de7490882fef4317851
Olha Bainurenova <okatranis@gmail.com>
2025-12-01 16:52:03 +0200

    feat: implement grid/list view toggle

--- src/js/script.js
+++ src/js/script.js
@@ -13,14 +13,14 @@ fetch('./src/data/posts.json')
           <div class="post-item__content">
             <h4 class="post-item__title">${post.title}</h4>
             <div class="post-item__stats">
-              <p class="post-item__likes">
+              <span class="post-item__likes">
                 <img src="./assets/img/heart-icon.svg" alt="likes" />
                 ${post.likes}
-              </p>
-              <p class="post-item__comments">
+              </span>
+              <span class="post-item__comments">
                 <img src="./assets/img/comment-icon.svg" alt="comments" />
                 ${post.comments}
-              </p>
+              </span>
             </div>
           </div>
         </div>
@@ -28,14 +28,14 @@ fetch('./src/data/posts.json')
         <div class="post-item__meta">
           <h4 class="post-item__title">${post.metaDate}</h4>
           <div class="post-item__stats">
-            <p class="post-item__likes">
+            <span class="post-item__likes">
               <img src="./assets/img/heart-icon.svg" alt="likes" />
               ${post.metaLikes}
-            </p>
-            <p class="post-item__comments">
+            </span>
+            <span class="post-item__comments">
               <img src="./assets/img/comment-icon.svg" alt="comments" />
               ${post.metaComments}
-            </p>
+            </span>
           </div>
         </div>
 
@@ -49,3 +49,24 @@ fetch('./src/data/posts.json')
     });
   })
   .catch(error => console.error('Error loading JSON:', error));
+
+
+  const postsListEl = document.getElementById("postsList");
+const gridBtn = document.getElementById("gridViewBtn");
+const listBtn = document.getElementById("listViewBtn");
+
+gridBtn.addEventListener("click", () => {
+  postsListEl.classList.remove("list-view");
+  postsListEl.classList.add("grid-view");
+
+  gridBtn.classList.add("view-toggle__active");
+  listBtn.classList.remove("view-toggle__active");
+});
+
+listBtn.addEventListener("click", () => {
+  postsListEl.classList.remove("grid-view");
+  postsListEl.classList.add("list-view");
+
+  listBtn.classList.add("view-toggle__active");
+  gridBtn.classList.remove("view-toggle__active");
+});
